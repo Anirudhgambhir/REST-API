47c785e0228693d3b3ed820fea2eb8dd32b4fc91
Anirudh Gambhir <anirugam@amazon.com>
2023-09-30 12:56:06 +0530

    Adding Exceptions

---
+++ src/main/java/taskManagement/exceptions/InvalidRequestException.java
@@ -0,0 +1,9 @@
+package taskManagement.exceptions;
+
+public class InvalidRequestException extends RuntimeException{
+
+    public InvalidRequestException(String message){
+        super(message);
+    }
+
+}
